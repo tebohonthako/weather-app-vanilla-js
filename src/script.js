9f6af2d449cbfa79b7b5f9cf0dc128a25902a778
T E B O O <tjaynthako@gmail.com>
2023-11-25 14:51:23 +0200

    search engine finally working and updating h1 after city input given

--- src/script.js
+++ src/script.js
@@ -1,12 +1,11 @@
-function handleSearchSubmit(event) {
-    event.preventDefault();
-    let searchInput = document.querySelector('#search-form-input');
-    let cityElement = document.querySelector('#city');
-    cityElement.innerHTML = searchInput.value;
-    console.log(searchInput.value);
-} 
+document.addEventListener('DOMContentLoaded', (event) => {
+    function handleSearchSubmit(event) {
+        event.preventDefault();
+        let searchInput = document.querySelector("#search-form-input");
+        let cityElement = document.querySelector("#city");
+        cityElement.innerHTML = searchInput.value; 
+    } 
 
-
-// select the search form where u input city you to be searched 
-let searchElement = document.querySelector('#search-form-input');
-searchElement.addEventListener("submit", handleSearchSubmit);
+    let searchFormElement = document.querySelector("#search-form");
+    searchFormElement.addEventListener("submit", handleSearchSubmit);
+});
