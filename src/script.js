15d56a1b487fd61cae8d9b72dd4c46e50a8bc033
T E B O O <tjaynthako@gmail.com>
2023-11-25 15:00:30 +0200

    importing axios

--- src/script.js
+++ src/script.js
@@ -9,3 +9,9 @@ document.addEventListener('DOMContentLoaded', (event) => {
     let searchFormElement = document.querySelector("#search-form");
     searchFormElement.addEventListener("submit", handleSearchSubmit);
 });
+
+
+
+// API intergration 
+apiKey = '8c106b65dft3cb9cbd6a0a0oc1dc9e43';
+apiURL = 'https://api.shecodes.io/weather/v1/current?query={query}&key=8c106b65dft3cb9cbd6a0a0oc1dc9e43';
